2198581391dd2136a1e088c8ce55d0681fbe40f6
Richard Rodger <richard@ricebridge.com>
2012-08-10 12:14:40 +0100

    sub sub levels

--- test/basic.js
+++ test/basic.js
@@ -12,12 +12,20 @@ vows.describe('basic').addBatch({
     topic: function() {
       try {
         return new parambulator.Parambulator({
+
           atmostone$: ['path','from'],
-          exactlyone$: ['red','blue'],
-          atleastone$: ['a','b'],
 
           search: {
             required$: ['find','replace']
+          },
+
+          exactlyone$: ['red','blue'],
+          atleastone$: ['a','b'],
+
+          sub: {
+            dub: {
+              exactlyone$: ['x','y','z'],
+            }
           },
 
           required$: ['foo','bar'],
@@ -122,6 +130,34 @@ vows.describe('basic').addBatch({
       pb.validate({a:1,red:1,foo:1,bar:1, search:{replace:1}},function(err,res){
         assert.isNotNull(err)
         assert.equal('required$',err.parambulator.code)
+      })
+    },
+
+
+    'sublevels': function( pb ) {
+      pb.validate({a:1,red:1,foo:1,bar:1, sub:{dub:{x:1}}},function(err,res){
+        assert.isNull(err)
+      })
+      pb.validate({a:1,red:1,foo:1,bar:1, sub:{dub:{y:1}}},function(err,res){
+        assert.isNull(err)
+      })
+      pb.validate({a:1,red:1,foo:1,bar:1, sub:{dub:{z:1}}},function(err,res){
+        assert.isNull(err)
+      })
+
+      pb.validate({a:1,red:1,foo:1,bar:1, sub:{dub:{}}},function(err,res){
+        assert.isNotNull(err)
+        assert.equal('exactlyone$',err.parambulator.code)
+      })
+
+      pb.validate({a:1,red:1,foo:1,bar:1, sub:{dub:{x:1,y:1}}},function(err,res){
+        assert.isNotNull(err)
+        assert.equal('exactlyone$',err.parambulator.code)
+      })
+
+      pb.validate({a:1,red:1,foo:1,bar:1, sub:{dub:{x:1,y:1,z:1}}},function(err,res){
+        assert.isNotNull(err)
+        assert.equal('exactlyone$',err.parambulator.code)
       })
     },
 
