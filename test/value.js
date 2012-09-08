225cc8bf66c780b3454e9faee41d073529e1a768
Marian Radulescu <marian.radulescu@gmail.com>
2012-09-09 00:57:29 +0300

    Removed uniq$ test from value.js (See #4)

--- test/value.js
+++ test/value.js
@@ -31,8 +31,6 @@ vows.describe('value').addBatch({
           q: {min$: new Date("2012-09-04")},
           r: {max$: 2},
           s: {max$: new Date("2012-09-04")},
-//          t: {uniq$: [1,2,3,4,5]},
-//          u: {uniq$: [1,2,3,4,5,1]},
           wild$:'top*', // does nothing
         })
       } 
