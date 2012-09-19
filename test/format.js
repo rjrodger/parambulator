452f3a7390faa735cfebedfd5ba37e76dd5335d1
mirceaalexandru <mircea.alexandru@gmail.com>
2012-09-19 16:13:55 +0300

    Implement support for format$ rule. Few formats are now available. See #7

--- test/format.js
+++ test/format.js
@@ -14,6 +14,9 @@ vows.describe('format').addBatch({
           a: {format$:'datetime'},
           b: {format$:'date'},
           c: {format$:'time'},
+          d: {format$:'utcmillisec'},
+          e: {format$:'re'},
+          f: {format$:['date', 'time']},
         })
       } 
       catch( e ) {
@@ -66,6 +69,82 @@ vows.describe('format').addBatch({
         assert.equal(err.parambulator.code,'format$')
       })
     },
+
+    'utcmillisec': function( pb ) {
+      pb.validate({},function(err,res){
+        assert.isNull(err)
+      })
+
+      pb.validate({d:124578},function(err,res){
+        assert.isNull(err)
+      })
+
+      pb.validate({d:'test'},function(err,res){
+        assert.isNotNull(err)
+        assert.equal(err.parambulator.code,'format$')
+      })
+    },
+    
+    're': function( pb ) {
+      pb.validate({},function(err,res){
+        assert.isNull(err)
+      })
+
+      pb.validate({e:/([0-1]\d|2[0-4]):[0-5]\d:[0-5]\dZ/},function(err,res){
+        assert.isNull(err)
+      })
+
+      pb.validate({e:124578},function(err,res){
+        assert.isNotNull(err)
+        assert.equal(err.parambulator.code,'format$')
+      })
+    },
+    
+
+    'date-time': function( pb ) {
+      pb.validate({},function(err,res){
+        assert.isNull(err)
+      })
+
+      pb.validate({f:'2012-02-02'},function(err,res){
+        assert.isNull(err)
+      })
+
+      pb.validate({f:'2012-32-02'},function(err,res){
+        assert.isNotNull(err)
+        assert.equal(err.parambulator.code,'format$')
+      })
+
+      pb.validate({f:'11:12:13Z'},function(err,res){
+        assert.isNull(err)
+      })
+
+      pb.validate({f:'51:12:13Z'},function(err,res){
+        assert.isNotNull(err)
+        assert.equal(err.parambulator.code,'format$')
+      })
+
+      pb.validate({f:124578},function(err,res){
+        assert.isNotNull(err)
+        assert.equal(err.parambulator.code,'format$')
+      })
+
+      pb.validate({f:/([0-1]\d|2[0-4]):[0-5]\d:[0-5]\dZ/},function(err,res){
+        assert.isNotNull(err)
+        assert.equal(err.parambulator.code,'format$')
+      })
+
+      pb.validate({f:124578},function(err,res){
+        assert.isNotNull(err)
+        assert.equal(err.parambulator.code,'format$')
+      })
+
+      pb.validate({f:'test'},function(err,res){
+        assert.isNotNull(err)
+        assert.equal(err.parambulator.code,'format$')
+      })
+    },
+    
   }
 }).export(module)
 
