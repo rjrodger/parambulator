ea900bd93873acd5840622faf4b2927a2ff164e3
Richard Rodger <richard@ricebridge.com>
2013-08-17 11:41:57 +0100

    multiple rulespecs, comma-separated

---
+++ test/string-rulespec.js
@@ -0,0 +1,51 @@
+/* Copyright (c) 2010-2013 Richard Rodger */
+
+"use strict";
+
+
+var assert = require('assert')
+
+var _      = require('underscore')
+
+
+
+var parambulator = require('..')
+
+
+
+describe('string-rulespec', function() {
+
+  it('single', function() {
+    var pm, res
+
+    pm = parambulator({foo:'required$'})
+    res = pm.validate({foo:'1'})
+    assert.ok( null == res )
+
+    res = pm.validate({bar:'1'})
+    assert.ok( null != res )
+    assert.equal('required$',res.parambulator.code)
+    assert.ok(res.parambulator.point.bar)
+  })
+
+
+  it('multiple', function() {
+    var pm, res
+
+    pm = parambulator({foo:'required$,integer$'})
+    res = pm.validate({foo:1})
+    assert.ok( null == res )
+
+    res = pm.validate({bar:1})
+    assert.ok( null != res )
+    assert.equal('required$',res.parambulator.code)
+    assert.ok(res.parambulator.point.bar)
+
+    res = pm.validate({foo:'zoo'})
+    assert.ok( null != res )
+    //console.log(res)
+    assert.equal('integer$',res.parambulator.code)
+    assert.ok(res.parambulator.point.foo)
+  })
+
+})
